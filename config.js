70b3773148bb16849e58730d00c0b05a2f7ce07a
imranmoko <imranmokoagow57@gmail.com>
2025-07-18 15:49:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 
-const TELEGRAM_TOKEN = "7238526151:AAFqvbQ7VVc7zonjAPjgDvtmPp-3CtQnY4o";
-const CHAT_ID = "7392241321";
+const TELEGRAM_TOKEN = "7691774076:AAGTA_gE5W6h_W7oj85UbknkQy-_3mUjU6k";
+const CHAT_ID = "7934452229";
